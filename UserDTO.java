41b24e4aeb7dd5569b0d62918858cc5677c164da
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-24 23:17:47 +0530

    Add files via upload

---
+++ UserDTO.java
@@ -0,0 +1,57 @@
+package com.wolken.hackerrank.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private long contactNumber;
+	public void setId(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setUsername(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setEmail(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setContactNumber(long contactNo) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setPassword(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public Object getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getContactNumber() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Object getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
