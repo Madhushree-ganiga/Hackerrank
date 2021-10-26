6ef0b6acf35395a59fa70be102bed903378bb280
Madhushree Ganiga/Javacode-Wolken <92204525+Madhushree-ganiga@users.noreply.github.com>
2021-10-26 10:09:18 +0530

    Add files via upload

---
+++ dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.wolken.hackerrank.dto;
+
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	
+	private int id;
+	private String username;
+	private String email;
+	private String password;
+	private String conpassword;
+	private long contactNumber;
+	
+	
+	
+}
